4cc5ef4ffb19180c4a2c2b0fed831811cb213866
Nick6154-dev <pbalsecamh5b@outlook.com>
2023-02-09 11:32:49 -0500

    First commit

--- src/main/java/uce/edu/ec/devengamiento/models/service/impl/PlanDevengamientoServiceImpl.java
+++ src/main/java/uce/edu/ec/devengamiento/models/service/impl/PlanDevengamientoServiceImpl.java
@@ -8,6 +8,7 @@ import uce.edu.ec.devengamiento.models.repository.IDocenteRepository;
 import uce.edu.ec.devengamiento.models.repository.IPlanDevengamientoRepository;
 import uce.edu.ec.devengamiento.models.service.IPlanDevengamientoService;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -26,7 +27,9 @@ public class PlanDevengamientoServiceImpl implements IPlanDevengamientoService {
 
     @Override
     public List<PlanDevengamiento> findByIdDocente(Long idDocente) {
-        return repository.findPlanDevengamientosByIdDocente(docenteRepository.findById(idDocente).orElse(new Docente()));
+        System.out.println(idDocente);
+        //return repository.findPlanDevengamientosByIdDocente(docenteRepository.findById(idDocente).orElse(new Docente()));
+        return Collections.emptyList();
     }
 
     @Override
