2dad542de830c57f77da3d584a2baea9d88181cc
Nick6154-dev <pbalsecamh5b@outlook.com>
2023-02-08 22:08:00 -0500

    First commit

--- src/main/java/uce/edu/ec/devengamiento/config/SpringSecurityConfig.java
+++ src/main/java/uce/edu/ec/devengamiento/config/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package uce.edu.ec.devengamiento.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import uce.edu.ec.devengamiento.auth.filter.JWTAuthenticationFilter;
 import uce.edu.ec.devengamiento.auth.filter.JWTAuthorizationFilter;
 import uce.edu.ec.devengamiento.auth.service.JWTService;
@@ -28,7 +32,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/accrual/authorization")
+        http.cors().and().authorizeRequests().antMatchers("/accrual/authorization")
                 .permitAll().anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtService))
@@ -42,4 +46,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         build.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedOrigin("http://localhost:5173");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 }
