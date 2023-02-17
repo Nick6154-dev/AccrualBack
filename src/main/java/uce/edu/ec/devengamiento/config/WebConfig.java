df63f1f641619dc2f08f64ea4e2cfb3ba29b2e3e
Nick6154-dev <pbalsecamh5b@outlook.com>
2023-02-17 17:47:17 -0500

    First commit

--- src/main/java/uce/edu/ec/devengamiento/config/WebConfig.java
+++ src/main/java/uce/edu/ec/devengamiento/config/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(link, "*")
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
