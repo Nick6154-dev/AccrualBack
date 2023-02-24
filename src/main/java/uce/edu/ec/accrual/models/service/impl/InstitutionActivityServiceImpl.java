f24b4fa440536d350111b6332259059cb1d95961
Nick6154-dev <pbalsecamh5b@outlook.com>
2023-02-24 12:32:18 -0500

    First commit

--- src/main/java/uce/edu/ec/accrual/models/service/impl/InstitutionActivityServiceImpl.java
+++ src/main/java/uce/edu/ec/accrual/models/service/impl/InstitutionActivityServiceImpl.java
@@ -37,25 +37,27 @@ public class InstitutionActivityServiceImpl implements InstitutionActivityServic
     @Override
     @Transactional
     public ResponseEntity<?> save(InstitutionActivity institutionActivity) {
-        Optional<University> university = universityService.findById(institutionActivity.getIdUniversity());
-        Optional<Faculty> faculty = facultyService.findById(institutionActivity.getIdFaculty());
-        Optional<Career> career = careerService.findById(institutionActivity.getIdCareer());
-        if (university.isPresent() && faculty.isPresent() && career.isPresent()) {
-            if (Objects.equals(university.get().getIdUniversity(), faculty.get().getUniversity().getIdUniversity()) &&
-                    Objects.equals(faculty.get().getIdFaculty(), career.get().getFaculty().getIdFaculty())) {
-                Institution institution = loadInstitution(institutionActivity, new Institution());
-                if (institutionActivity.getInstitutionName().equals("Universidad Central del Ecuador")) {
+        if (institutionActivity.getInstitutionName().equals("Universidad Central del Ecuador")) {
+            Optional<University> university = universityService.findById(institutionActivity.getIdUniversity());
+            Optional<Faculty> faculty = facultyService.findById(institutionActivity.getIdFaculty());
+            Optional<Career> career = careerService.findById(institutionActivity.getIdCareer());
+            if (university.isPresent() && faculty.isPresent() && career.isPresent()) {
+                if (Objects.equals(university.get().getIdUniversity(), faculty.get().getUniversity().getIdUniversity()) &&
+                        Objects.equals(faculty.get().getIdFaculty(), career.get().getFaculty().getIdFaculty())) {
+                    Institution institution = loadInstitution(institutionActivity, new Institution());
                     loadUniversityInstitution(institutionActivity, institution, new UniversityInstitution());
-                } else {
-                    loadOtherInstitution(institutionActivity, institution, new OtherInstitution());
+                    return ResponseEntity.status(HttpStatus.CREATED).body("Universidad guardada con exito");
                 }
-                return ResponseEntity.status(HttpStatus.CREATED).body("Datos guardados con exito");
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Los tipos de universidad facultad y carrera " +
+                        "no concuerdan entre ellos");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("La universidad, facultad o carrera no han " +
+                        "sido encontrados en el sistema");
             }
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Los tipos de universidad facultad y carrera " +
-                    "no concuerdan entre ellos");
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("La universidad, facultad o carrera no han " +
-                    "sido encontrados en el sistema");
+            Institution institution = loadInstitution(institutionActivity, new Institution());
+            loadOtherInstitution(institutionActivity, institution, new OtherInstitution());
+            return ResponseEntity.status(HttpStatus.CREATED).body("Otra institucion guardada con exito");
         }
     }
 
@@ -78,9 +80,6 @@ public class InstitutionActivityServiceImpl implements InstitutionActivityServic
     @Transactional
     public ResponseEntity<?> update(InstitutionActivity institutionActivity, Long idInstitution) {
         return institutionService.findById(idInstitution).map(value -> {
-            Optional<University> university = universityService.findById(institutionActivity.getIdUniversity());
-            Optional<Faculty> faculty = facultyService.findById(institutionActivity.getIdFaculty());
-            Optional<Career> career = careerService.findById(institutionActivity.getIdCareer());
             if (!institutionActivity.getInstitutionName().equals(value.getInstitutionName())) {
                 if (institutionActivity.getInstitutionName().equals("Universidad Central del Ecuador")) {
                     otherInstitutionService.deleteOtherInstitutionByInstitution(value);
@@ -88,14 +87,18 @@ public class InstitutionActivityServiceImpl implements InstitutionActivityServic
                     universityInstitutionService.deleteUniversityInstitutionByInstitution(value);
                 }
             }
-            Institution institution = loadInstitution(institutionActivity, value);
             if (institutionActivity.getInstitutionName().equals("Universidad Central del Ecuador")) {
+                Optional<University> university = universityService.findById(institutionActivity.getIdUniversity());
+                Optional<Faculty> faculty = facultyService.findById(institutionActivity.getIdFaculty());
+                Optional<Career> career = careerService.findById(institutionActivity.getIdCareer());
                 if (university.isPresent() && faculty.isPresent() && career.isPresent()) {
                     if (Objects.equals(university.get().getIdUniversity(), faculty.get().getUniversity().getIdUniversity()) &&
                             Objects.equals(faculty.get().getIdFaculty(), career.get().getFaculty().getIdFaculty())) {
+                        Institution institution = loadInstitution(institutionActivity, value);
                         loadUniversityInstitution(institutionActivity, institution,
                                 universityInstitutionService.findUniversityInstitutionByInstitution(institution)
                                         .orElse(new UniversityInstitution()));
+                        return ResponseEntity.status(HttpStatus.CREATED).body("Datos actualizados con exito");
                     } else {
                         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Los tipos de universidad facultad y carrera " +
                                 "no concuerdan entre ellos");
@@ -105,11 +108,12 @@ public class InstitutionActivityServiceImpl implements InstitutionActivityServic
                             "sido encontrados en el sistema");
                 }
             } else {
+                Institution institution = loadInstitution(institutionActivity, value);
                 loadOtherInstitution(institutionActivity, institution,
                         otherInstitutionService.findOtherInstitutionByInstitution(institution)
                                 .orElse(new OtherInstitution()));
+                return ResponseEntity.status(HttpStatus.CREATED).body("Datos actualizados con exito");
             }
-            return ResponseEntity.status(HttpStatus.CREATED).body("Datos actualizados con exito");
         }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("No se pudo encontrar un objeto asignado " +
                 "a esa id"));
     }
@@ -122,22 +126,22 @@ public class InstitutionActivityServiceImpl implements InstitutionActivityServic
     }
 
     @Transactional
-    public OtherInstitution loadOtherInstitution(InstitutionActivity institutionActivity, Institution institution,
+    public void loadOtherInstitution(InstitutionActivity institutionActivity, Institution institution,
                                                  OtherInstitution otherInstitution) {
         otherInstitution.setOtherName(institutionActivity.getOtherInstitutionName());
         otherInstitution.setInstitution(institution);
         otherInstitution.setVerificationLink(institutionActivity.getVerificationLink());
-        return otherInstitutionService.save(otherInstitution);
+        otherInstitutionService.save(otherInstitution);
     }
 
     @Transactional
-    public UniversityInstitution loadUniversityInstitution(InstitutionActivity institutionActivity, Institution institution,
+    public void loadUniversityInstitution(InstitutionActivity institutionActivity, Institution institution,
                                                            UniversityInstitution universityInstitution) {
         universityInstitution.setInstitution(institution);
         universityInstitution.setUniversity(universityService.findById(institutionActivity.getIdUniversity()).orElse(new University()));
         universityInstitution.setFaculty(facultyService.findById(institutionActivity.getIdFaculty()).orElse(new Faculty()));
         universityInstitution.setCareer(careerService.findById(institutionActivity.getIdCareer()).orElse(new Career()));
-        return universityInstitutionService.save(universityInstitution);
+        universityInstitutionService.save(universityInstitution);
     }
 
 }
